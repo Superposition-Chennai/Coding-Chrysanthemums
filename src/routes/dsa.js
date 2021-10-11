23c55d6b513b0c1025257fcdc77e0468470c52fd
Rakshaa Viswanathan <46165429+rakshaa2000@users.noreply.github.com>
2021-10-11 11:45:49 +0530

    added resources

--- src/routes/dsa.js
+++ src/routes/dsa.js
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Card } from "@material-ui/core";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faYoutube } from "@fortawesome/free-brands-svg-icons";
 import title from "../img/Software engineer-cuate.png";
 export default function DSA() {
   return (
@@ -5,14 +9,112 @@ export default function DSA() {
       <div
         className="App"
         id="head"
-        style={{ justifyContent: "center", display: "flex", flexWrap: "wrap" }}
+        style={{
+          alignContent: "center",
+          justifyContent: "center",
+          display: "flex",
+          flexWrap: "wrap"
+        }}
       >
-        <div style={{ fontFamily: "Poppins" }}>
+        <div
+          style={{
+            fontFamily: "Poppins",
+            width: "100%",
+            alignContent: "center"
+          }}
+        >
           <h1>DSA Resources</h1>
           <img src={title} alt="title" width="200px" />
+          <div
+            style={{
+              display: "flex",
+              alignContent: "center",
+              justifyContent: "flex-start",
+              margin: "auto",
+              flexWrap: "wrap",
+              width: "100%"
+            }}
+          >
+            <div className="item">
+              <Card className="Card1">
+                <h4>DS Collection</h4>
+                <p>
+                  Geeks for Geeks{" "}
+                  <a href="https://www.geeksforgeeks.org/data-structures">
+                    <FontAwesomeIcon icon={faLink} />
+                  </a>
+                </p>
+              </Card>
+            </div>
+            <div className="item">
+              <Card className="Card1">
+                <h4>Algo Collection</h4>
+                <p>
+                  Geeks for Geeks{" "}
+                  <a href="https://www.geeksforgeeks.org/fundamentals-of-algorithms/">
+                    <FontAwesomeIcon icon={faLink} />
+                  </a>
+                </p>
+              </Card>
+            </div>
+            <div className="item">
+              <Card className="Card1">
+                <h4>Learn DSA</h4>
+                <p>
+                  Programiz{" "}
+                  <a href="https://www.programiz.com/dsa">
+                    <FontAwesomeIcon icon={faLink} />
+                  </a>
+                </p>
+              </Card>
+            </div>
+            <div className="item">
+              <Card className="Card1">
+                <h4>Algorithms Notes</h4>
+                <p>
+                  GoalKicker{" "}
+                  <a href="https://books.goalkicker.com/AlgorithmsBook/">
+                    <FontAwesomeIcon icon={faLink} />
+                  </a>
+                </p>
+              </Card>
+            </div>
+            <div className="item">
+              <Card className="Card1">
+                <h4>DSA with Java</h4>
+                <p>
+                  Community Classroom{" "}
+                  <a href="https://www.youtube.com/playlist?list=PL9gnSGHSqcnr_DxHsP7AW9ftq0AtAyYqJ">
+                    <FontAwesomeIcon icon={faYoutube} />
+                  </a>
+                </p>
+              </Card>
+            </div>
+            <div className="item">
+              <Card className="Card1">
+                <h4>C++ Algorithms</h4>
+                <p>
+                  The Algorithms{" "}
+                  <a href="https://github.com/TheAlgorithms/C-Plus-Plus/">
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                </p>
+              </Card>
+            </div>
+            <div className="item">
+              <Card className="Card1">
+                <h4>Python Algorithms</h4>
+                <p>
+                  The Algorithms{" "}
+                  <a href="https://github.com/TheAlgorithms/Python/">
+                    <FontAwesomeIcon icon={faGithub} />
+                  </a>
+                </p>
+              </Card>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="App"></div>
     </>
   );
 }
