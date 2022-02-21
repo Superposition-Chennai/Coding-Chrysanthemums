2f6ce9ba6579d76f7eeff0f015c1077683df2168
Rakshaa Viswanathan <46165429+rakshaa2000@users.noreply.github.com>
2022-02-21 19:51:36 +0530

    new features

--- src/routes/womentors.js
+++ src/routes/womentors.js
@@ -8,7 +8,7 @@ export default function Womentors(){
           }}>
               <h2>Womentors</h2>
               <p>The premier mentorship program of Superposition Chennai for Underrepresented Genders</p>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd4POiZaxm72SE4V6fb2ql7cctPcz_N2_24gbF7dbCUzxRf4A/viewform?embedded=true" width="640" height="2285" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd4POiZaxm72SE4V6fb2ql7cctPcz_N2_24gbF7dbCUzxRf4A/viewform?embedded=true" width="100%" height="2285" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
     );
 }
